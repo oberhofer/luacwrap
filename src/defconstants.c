c3e6bad8ccb6517c3b6f715a95568d6282736121
Klaus Oberhofer <oberhofer@users.sourceforge.net>
2011-06-21 00:36:42 +0200

    added helper function to define UINT constants (enums)

---
+++ src/defconstants.c
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////////////////////////
+/**
+
+  @file   "lua_constdef.cpp"
+
+  @short  Lua utilities
+
+  @author Klaus Oberhofer
+
+  Utility functions to register constants
+
+*/////////////////////////////////////////////////////////////////////////
+
+#include "luacwrap.h"
+
+//////////////////////////////////////////////////////////////////////////
+/**
+
+  Registers a list of global constants
+
+  @param[in]  L         lua state
+  @param[in]  constants constant table
+
+*/////////////////////////////////////////////////////////////////////////
+void luacwrap_defuintconstants(lua_State* L, luacwrap_DefUIntConst* constants)
+{
+  if (constants)
+  {
+    while (constants->name)
+    {
+      lua_pushinteger(L, constants->value);
+      lua_setglobal  (L, constants->name);
+
+      ++constants;
+    }
+  }
+}
+
