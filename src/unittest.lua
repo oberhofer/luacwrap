62e25f43110389209a2e13b27fb6041c9a603b41
Klaus Oberhofer <oberhofer@users.sourceforge.net>
2021-08-17 17:30:40 +0200

    use luaunit as unit test framework

--- src/unittest.lua
+++ src/unittest.lua
@@ -1,339 +1,363 @@
+lu = require("luaunit")
 testluacwrap = require("testluacwrap")
-
--- helper functions
-function printtable(t)
-  for k,v in pairs(t) do
-    print("  ", k, v)
-  end
-end
-
-print("--> create TESTSTRUCT")
-local struct = TESTSTRUCT:new()
-
--- print(debug.getuservalue(struct))
--- printtable(debug.getuservalue(struct))
-
-
-print("--> semantic of attach()")
-local attached = TESTSTRUCT:attach(struct)
-assert(struct.__ptr == attached.__ptr)
-
-print("--> checkInnerStructAccess")
-print(getmetatable(struct))
-for k, v in pairs(getmetatable(struct)) do
-	print(k, v)
-end
-print("----")
-print(getmetatable(struct.inner))
-for k, v in pairs(getmetatable(struct.inner)) do
-	print(k, v)
-end
-print("----")
-
-assert(1 == testluacwrap.checkInnerStructAccess(struct, struct.inner))
-
-print("--> check $ref init values")
-
--- by default references should have the value nil
-assert(nil == struct.ref.value)
--- and the reserved index 0
-assert(0 == struct.ref.ref)
-
--- print adresses
-print("--> print adresses (__ptr)")
-print("struct.__ptr", struct.__ptr)
-print("struct.intarray.__ptr", struct.intarray.__ptr)
-print("struct.inner.__ptr", struct.inner.__ptr)
-
--- print psztext member
-print("struct.inner.pszText", struct.inner.pszText)
-
-print("--> test assignment")
-struct.u8  = 11
-struct.i8  = 22
-struct.u16 = 33
-struct.i16 = 44
-struct.u32 = 55
-struct.i32 = 66
-struct.ptr = "hello"
-struct.chararray = "hello"
-struct.intarray[1] = 10
-struct.intarray[2] = 20
-struct.intarray[3] = 30
-struct.intarray[4] = 40
-
--- check assigned values
-assert(struct.u8  == 11)
-assert(struct.i8  == 22)
-assert(struct.u16 == 33)
-assert(struct.i16 == 44)
-assert(struct.u32 == 55)
-assert(struct.i32 == 66)
-assert(struct.ptr == "hello")
-assert(struct.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
-assert(struct.intarray[1] == 10)
-assert(struct.intarray[2] == 20)
-assert(struct.intarray[3] == 30)
-assert(struct.intarray[4] == 40)
-
-print("--> test pointer reset (reference removal)")
-
-struct.ptr = "hello2"
-assert(struct.ptr == "hello2")
-
-struct.ptr = nil
-assert(struct.ptr == nil)
-
-struct.ptr = "hello2"
-assert(struct.ptr == "hello2")
-
-struct.ptr = 0
-assert(struct.ptr == nil)
-
-print("--> test table assignment")
-struct:set{
-	u8  = 91,
-	i8  = 92,
-	u16 = 93,
-	i16 = 94,
-	u32 = 95,
-	i32 = 96,
-	ptr = "hello",
-	chararray = "hello",
-	intarray = { 19,
-				 29,
- 				 39,
- 				 49,
-	},
-}
-
-assert(struct.u8  == 91)
-assert(struct.i8  == 92)
-assert(struct.u16 == 93)
-assert(struct.i16 == 94)
-assert(struct.u32 == 95)
-assert(struct.i32 == 96)
-assert(struct.ptr == "hello")
-assert(struct.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
-assert(struct.intarray[1] == 19)
-assert(struct.intarray[2] == 29)
-assert(struct.intarray[3] == 39)
-assert(struct.intarray[4] == 49)
-
-print("--> test new with table assignment")
-struct = TESTSTRUCT:new{
-	u8  = 91,
-	i8  = 92,
-	u16 = 93,
-	i16 = 94,
-	u32 = 95,
-	i32 = 96,
-	ptr = "hello",
-	chararray = "hello",
-	intarray = { 19,
-				 29,
- 				 39,
- 				 49,
-	}
-}
-
-assert(struct.u8  == 91)
-assert(struct.i8  == 92)
-assert(struct.u16 == 93)
-assert(struct.i16 == 94)
-assert(struct.u32 == 95)
-assert(struct.i32 == 96)
-assert(struct.ptr == "hello")
-assert(struct.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
-assert(struct.intarray[1] == 19)
-assert(struct.intarray[2] == 29)
-assert(struct.intarray[3] == 39)
-assert(struct.intarray[4] == 49)
-
-print("struct.inner.pszText", struct.inner.pszText)
-
---[[
-print("--> test new with assignment")
-struct2 = TESTSTRUCT:new(struct)
-
-print(struct2)
-
-assert(struct2.u8  == 91)
-assert(struct2.i8  == 92)
-assert(struct2.u16 == 93)
-assert(struct2.i16 == 94)
-assert(struct2.u32 == 95)
-assert(struct2.i32 == 96)
-assert(struct2.ptr == "hello")
-assert(struct2.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
-assert(struct2.intarray[1] == 19)
-assert(struct2.intarray[2] == 29)
-assert(struct2.intarray[3] == 39)
-assert(struct2.intarray[4] == 49)
---]]
-
-print("--> test new with assignment")
-for k, v in pairs(getmetatable(struct)) do
-	print(k, v)
-end
-
-
-
-struct2 = struct:__dup()
-
-print(struct2)
-
-assert(struct2.u8  == 91)
-assert(struct2.i8  == 92)
-assert(struct2.u16 == 93)
-assert(struct2.i16 == 94)
-assert(struct2.u32 == 95)
-assert(struct2.i32 == 96)
-assert(struct2.ptr == "hello")
-assert(struct2.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
-assert(struct2.intarray[1] == 19)
-assert(struct2.intarray[2] == 29)
-assert(struct2.intarray[3] == 39)
-assert(struct2.intarray[4] == 49)
-
-
--- check sizes
-print("--> check __len")
--- assert(#struct == 80)       -- depends on packing, could be 72
-print("struct size: ", #struct)
-assert(#struct.intarray == 4)
-assert(#struct.chararray == 32)
-
-print("--> print")
-print(struct)
-print("struct.__ptr", struct.__ptr)
-print("struct.inner.__ptr", struct.inner.__ptr)
-print("struct.ptr", struct.ptr)
-print("struct.inner.pszText", struct.inner.pszText)
-
-print("--> internal print")
-print(testluacwrap.printTESTSTRUCT(struct))
-
-
-print("--> check $ref types")
-struct.ref = "my ref"
--- check internal reference assignment
-assert(struct.ref.ref == 1)
-
-assert("string" == type(struct.ref.value))
-assert("my ref" == struct.ref.value)
-
-print("--> test attach")
--- test attach
-function myfunc(struct)
-  print("within myfunc")
-  print(struct)
-
-  local wrap = TESTSTRUCT:attach(struct)
-  print(wrap)
-  -- printtable(debug.getuservalue(wrap))
-
-  print("wrap.__ptr", wrap.__ptr)
-  print(testluacwrap.printTESTSTRUCT(wrap))
-end
-testluacwrap.callwithTESTSTRUCT(myfunc)
-
-print("--> semantic of attach() with light embedded structs")
-function myfunc(struct)
-  local wrap = TESTSTRUCT:attach(struct)
-  -- print(wrap)
-  
-  local attached = INNERSTRUCT:attach(wrap)
-  -- print(attached)
-
-  -- check if they point to the same memory
-  assert(wrap.__ptr == attached.__ptr)  
-end
-testluacwrap.callwithTESTSTRUCT(myfunc)
-
-print("--> test boxed")
-function myfunc(struct)
-  print(struct)
-  -- print(debug.getuservalue(struct))
-  -- printtable(debug.getuservalue(struct))
-  print("struct.__ptr", struct.__ptr)
-  print(testluacwrap.printTESTSTRUCT(struct))
-end
-testluacwrap.callwithBoxedTESTSTRUCT(myfunc)
-
-
-print("--> callwithwrappedTESTSTRUCT")
--- test wrapped stack based objects
-function wrapfunc(wrap)
-  print(wrap)
-end
-testluacwrap.callwithwrappedTESTSTRUCT(wrapfunc)
-
-
-print("--> callwithRefType")
--- test wrapped stack based objects
-function wrapfunc(wrap)
-  print(wrap)
-
-  assert(wrap.u8  ==   8)
-  assert(wrap.i8  == - 8)
-  assert(wrap.u16 ==  16)
-  assert(wrap.i16 == -16)
-  assert(wrap.u32 ==  32)
-  assert(wrap.i32 == -32)
-
-  print("wrap.__ptr", wrap.__ptr)
-
-  assert("string" == type(wrap.ref.value))
-  assert("callwithRefType" == wrap.ref.value)
-
-  print(testluacwrap.printTESTSTRUCT(wrap))
-end
-testluacwrap.callwithRefType(wrapfunc, "callwithRefType")
-
-
 luacwrap = require("luacwrap")
 
-print("--> checkbuffers")
-function check_buffers()
-    local mybuf = luacwrap.createbuffer(256)
-    print(mybuf)
-    local mybuf2 = luacwrap.createbuffer(256)
-    print(mybuf2)
 
-    -- create instance
-    mybuf = "hello"
-    print("content of mybuf:", mybuf)
+local function getTable(t)
+  local tmp = {}
+  for k, v in pairs(t) do
+    tmp[#tmp+1] = type(v) .. " " .. k
+  end
+  return table.concat(tmp, ", ")
 end
-check_buffers()
 
-print("--> checkarrays")
-function check_arrays()
-    -- create type descriptor
-    local type_double128 = luacwrap.registerarray("double128", 128, "$dbl")
-    -- create instance
-    local myarray = type_double128:new()
-    print(myarray)
-end
-check_arrays()
+TestTESTSTRUCT = {}
+    function TestTESTSTRUCT:testCreateTESTSTRUCT()
+        local struct = TESTSTRUCT:new()
+        assert(nil ~= struct)
 
-print("--> checkstructs")
-function check_structs()
-    -- create type descriptor
-    local type_mystruct = luacwrap.registerstruct("mystruct", 8,
-      {
-        { "member1", 0, "$i32" },
-        { "member2", 4, "$i32" }
-      }
-    )
-    -- create struct instance
-    local mystruct = type_mystruct:new()
-    -- access members
-    mystruct.member1 = 10
-    mystruct.member2 = 22
-    print(mystruct)
-end
-check_structs()
+        -- semantic of attach()"
+        local attached = TESTSTRUCT:attach(struct)
+        assertEquals(attached, struct)
+        assertEquals(struct.__ptr, attached.__ptr)
+        
+        -- check metatable
+        assert(nil ~= getmetatable(struct))
+        assert(getTable(getmetatable(struct)) == [[userdata getouter, function __len, function __tostring, function __index, function __newindex]])
+        
+        -- check inner struct access
+        assert(nil ~= getmetatable(struct.inner))
+        assert(getTable(getmetatable(struct.inner)) == [[userdata getouter, function __gc, function __len, function __tostring, function __index, function __newindex]])
+                                                   
+        -- print(getTable(getmetatable(struct.inner)))
+        
+        assert(1 == testluacwrap.checkInnerStructAccess(struct, struct.inner))
+
+        -- check $ref init values
+        -- by default references should have the value nil
+        assert(nil == struct.ref.value)
+
+        -- and the reserved index 0
+        assert(0 == struct.ref.ref)
+
+        -- access __ptr
+        assert("userdata" == type(struct.__ptr))
+        assert("userdata" == type(struct.intarray.__ptr))
+        assert("userdata" == type(struct.inner.__ptr))
+
+        -- access psztext member
+        assert("nil" == type(struct.inner.pszText))
+        assert(nil   == struct.inner.pszText)
+    end
+
+    function TestTESTSTRUCT:testAssignment()
+        local struct = TESTSTRUCT:new()
+
+        struct.u8  = 11
+        struct.i8  = 22
+        struct.u16 = 33
+        struct.i16 = 44
+        struct.u32 = 55
+        struct.i32 = 66
+        struct.ptr = "hello"
+        struct.chararray = "hello"
+        struct.intarray[1] = 10
+        struct.intarray[2] = 20
+        struct.intarray[3] = 30
+        struct.intarray[4] = 40
+
+        -- check assigned values
+        assert(struct.u8  == 11)
+        assert(struct.i8  == 22)
+        assert(struct.u16 == 33)
+        assert(struct.i16 == 44)
+        assert(struct.u32 == 55)
+        assert(struct.i32 == 66)
+        assert(struct.ptr == "hello")
+        assert(struct.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+        assert(struct.intarray[1] == 10)
+        assert(struct.intarray[2] == 20)
+        assert(struct.intarray[3] == 30)
+        assert(struct.intarray[4] == 40)
+    end
+
+    function TestTESTSTRUCT:testReference()
+        local struct = TESTSTRUCT:new()
+        
+        -- test pointer reset (reference removal)
+        struct.ptr = "hello2"
+        assert(struct.ptr == "hello2")
+
+        struct.ptr = nil
+        assert(struct.ptr == nil)
+
+        struct.ptr = "hello2"
+        assert(struct.ptr == "hello2")
+
+        struct.ptr = 0
+        assert(struct.ptr == nil)
+    end
+
+    function TestTESTSTRUCT:testTableAssignment()
+        local struct = TESTSTRUCT:new()
+        
+        -- test table assignment
+        struct:set{
+          u8  = 91,
+          i8  = 92,
+          u16 = 93,
+          i16 = 94,
+          u32 = 95,
+          i32 = 96,
+          ptr = "hello",
+          chararray = "hello",
+          intarray = { 19,
+                 29,
+                 39,
+                 49,
+          },
+        }
+
+        assert(struct.u8  == 91)
+        assert(struct.i8  == 92)
+        assert(struct.u16 == 93)
+        assert(struct.i16 == 94)
+        assert(struct.u32 == 95)
+        assert(struct.i32 == 96)
+        assert(struct.ptr == "hello")
+        assert(struct.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+        assert(struct.intarray[1] == 19)
+        assert(struct.intarray[2] == 29)
+        assert(struct.intarray[3] == 39)
+        assert(struct.intarray[4] == 49)
+    end
 
 
-print("--> all checks passed")
+    function TestTESTSTRUCT:testNewWithTableAssignment()
+        
+        -- test new with table assignment
+        local struct = TESTSTRUCT:new{
+          u8  = 91,
+          i8  = 92,
+          u16 = 93,
+          i16 = 94,
+          u32 = 95,
+          i32 = 96,
+          ptr = "hello",
+          chararray = "hello",
+          intarray = { 19,
+                 29,
+                 39,
+                 49,
+          }
+        }
+
+        assert(struct.u8  == 91)
+        assert(struct.i8  == 92)
+        assert(struct.u16 == 93)
+        assert(struct.i16 == 94)
+        assert(struct.u32 == 95)
+        assert(struct.i32 == 96)
+        assert(struct.ptr == "hello")
+        assert(struct.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+        assert(struct.intarray[1] == 19)
+        assert(struct.intarray[2] == 29)
+        assert(struct.intarray[3] == 39)
+        assert(struct.intarray[4] == 49)
+    
+        assert(nil == struct.inner.pszText)
+        
+        -- test new with assignment from existing struct
+        local struct2 = TESTSTRUCT:new(struct)
+
+        assert(struct2.u8  == 91)
+        assert(struct2.i8  == 92)
+        assert(struct2.u16 == 93)
+        assert(struct2.i16 == 94)
+        assert(struct2.u32 == 95)
+        assert(struct2.i32 == 96)
+        assert(struct2.ptr == "hello")
+        assert(struct2.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+        assert(struct2.intarray[1] == 19)
+        assert(struct2.intarray[2] == 29)
+        assert(struct2.intarray[3] == 39)
+        assert(struct2.intarray[4] == 49)
+
+        assert(nil == struct2.inner.pszText)
+
+        
+        -- test __dup() from existing struct
+        local struct3 = struct:__dup()
+
+        assert(struct3.u8  == 91)
+        assert(struct3.i8  == 92)
+        assert(struct3.u16 == 93)
+        assert(struct3.i16 == 94)
+        assert(struct3.u32 == 95)
+        assert(struct3.i32 == 96)
+        assert(struct3.ptr == "hello")
+        assert(struct3.chararray == "hello\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+        assert(struct3.intarray[1] == 19)
+        assert(struct3.intarray[2] == 29)
+        assert(struct3.intarray[3] == 39)
+        assert(struct3.intarray[4] == 49)
+    end
+
+    function TestTESTSTRUCT:testSizes()
+
+        local struct = TESTSTRUCT:new()
+    
+        -- check sizes
+        assert(#struct == 76)             -- depends on packing
+        
+        assert(#struct.intarray == 4)
+        assert(#struct.chararray == 32)
+        
+        assert(0 == struct.ref.ref)
+        assert(nil == struct.ref.value)
+
+        -- check $ref types
+        struct.ref = "my unique reference string"
+
+        assert(0 ~= struct.ref.ref)
+
+        assert("string" == type(struct.ref.value))
+        assert("my unique reference string" == struct.ref.value)
+    end
+
+    function TestTESTSTRUCT:testAttach()
+        -- test attach
+        function myfunc(s)
+          local wrap = TESTSTRUCT:attach(s)
+
+          assert(nil ~= wrap.ptr)
+          
+          assert(wrap.u8  ==   8)
+          assert(wrap.i8  == - 8)
+          assert(wrap.u16 ==  16)
+          assert(wrap.i16 == -16)
+          assert(wrap.u32 ==  32)
+          assert(wrap.i32 == -32)
+
+          -- print(wrap)
+          
+        end
+        testluacwrap.callwithTESTSTRUCT(myfunc)
+    end
+
+    function TestTESTSTRUCT:testAttachWithLightEmbeddedStructs()
+
+        -- semantic of attach() with light embedded structs")
+        function myfunc(struct)
+          local wrap = TESTSTRUCT:attach(struct)
+          
+          local attached = INNERSTRUCT:attach(wrap)
+
+          -- check if they point to the same memory
+          assert(wrap.__ptr == attached.__ptr)  
+        end
+        testluacwrap.callwithTESTSTRUCT(myfunc)
+    end
+
+    function TestTESTSTRUCT:testBoxed()
+        -- test boxed
+        function myfunc(struct)
+          -- print(struct)
+          -- print(testluacwrap.printTESTSTRUCT(struct))
+          
+          assert(struct.u8  ==   8)
+          assert(struct.i8  == - 8)
+          assert(struct.u16 ==  16)
+          assert(struct.i16 == -16)
+          assert(struct.u32 ==  32)
+          assert(struct.i32 == -32)
+        end
+        testluacwrap.callwithBoxedTESTSTRUCT(myfunc)
+    end
+
+    function TestTESTSTRUCT:testCallwithwrappedTESTSTRUCT()
+
+        -- callwithwrappedTESTSTRUCT")
+        -- test wrapped stack based objects
+        function wrapfunc(wrap)
+          -- print(wrap)
+          assert(wrap.u8  ==   8)
+          assert(wrap.i8  == - 8)
+          assert(wrap.u16 ==  16)
+          assert(wrap.i16 == -16)
+          assert(wrap.u32 ==  32)
+          assert(wrap.i32 == -32)
+          
+        end
+        testluacwrap.callwithwrappedTESTSTRUCT(wrapfunc)
+    end
+
+
+    function TestTESTSTRUCT:testCallwithRefType()
+
+        -- callwithRefType
+        -- test wrapped stack based objects
+        function wrapfunc(wrap)
+          -- print(wrap)
+
+          assert(wrap.u8  ==   8)
+          assert(wrap.i8  == - 8)
+          assert(wrap.u16 ==  16)
+          assert(wrap.i16 == -16)
+          assert(wrap.u32 ==  32)
+          assert(wrap.i32 == -32)
+
+          -- print("wrap.__ptr", wrap.__ptr)
+          assert(0 ~= wrap.ref.ref)
+
+          assert("string" == type(wrap.ref.value))
+          assert("callwithRefType" == wrap.ref.value)
+
+          -- print(testluacwrap.printTESTSTRUCT(wrap))
+        end
+        testluacwrap.callwithRefType(wrapfunc, "callwithRefType")
+    end
+
+
+    function TestTESTSTRUCT:testBuffers()
+        -- checkbuffers
+        local mybuf = luacwrap.createbuffer(256)
+        -- print(mybuf)
+        local mybuf2 = luacwrap.createbuffer(256)
+        -- print(mybuf2)
+
+        -- create instance
+        mybuf = "hello"
+        
+        assert("hello" == mybuf)
+        -- print("content of mybuf:", mybuf)
+    end
+
+
+    function TestTESTSTRUCT:testArrays()
+        -- create type descriptor
+        local type_double128 = luacwrap.registerarray("double128", 128, "$dbl")
+        -- create instance
+        local myarray = type_double128:new()
+        -- print(myarray)
+    end
+
+
+    function TestTESTSTRUCT:testStructs()
+        -- create type descriptor
+        local type_mystruct = luacwrap.registerstruct("mystruct", 8,
+          {
+            { "member1", 0, "$i32" },
+            { "member2", 4, "$i32" }
+          }
+        )
+        -- create struct instance
+        local mystruct = type_mystruct:new()
+        -- access members
+        mystruct.member1 = 10
+        mystruct.member2 = 22
+        -- print(mystruct)
+    end
+
+    
+os.exit(lu.run())
